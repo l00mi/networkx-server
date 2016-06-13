a5332d1dc23f61324bf1cee4e234478d026dbb03
Michael Luggen <michael@wifi-secure-150-246.unifr.ch>
2016-06-13 16:33:19 +0200

    without server object

--- graph_builder.py
+++ graph_builder.py
@@ -3,11 +3,10 @@ import ijson
 import gzip
 import random
 
-config.VERSION="20160502"
-config.PATH="/storage/datasets/"
+import config
 
 def build_graph():
-    parser = ijson.parse(bz2.open(config.PATH+'wikidata-'+config.VERSION+'-all.json.bz2', mode = 'rb'))
+#    parser = ijson.parse(bz2.open(config.PATH+'wikidata-'+config.VERSION+'-all.json.bz2', mode = 'rb'))
     G = nx.Graph()
     G.add_node(1)
     G.add_nodes_from([2,3])
--- zerorpc_server.py
+++ zerorpc_server.py
@@ -2,17 +2,16 @@ import multiprocessing
 import zerorpc
 from pprint import pprint
 
-class Server(object):
-    def __init__(self, port, graph):
-        self.port = port
-        self.graph = graph
-    def edges(self):
-        return self.graph.edges()
-
+#class Server(object):
+#    def __init__(self, port, graph):
+#        self.port = port
+#        self.graph = graph
+#    def edges(self):
+#        return self.graph.edges()
 
 def serve(port, manager):
     print("Starting RCP server on port {}".format(port))
-    server = Server(port, manager.get_graph())
-    s = zerorpc.Server(server)
+#    server = Server(port, manager.get_graph())
+    s = zerorpc.Server(manager.get_graph())
     s.bind("tcp://0.0.0.0:{}".format(port))
     s.run()
