66280cea8e6e49de1443ab74ff63168723c4bbd3
Michael Luggen <michael@wifi-secure-150-246.unifr.ch>
2016-06-13 15:35:28 +0200

    import libs

--- graph_builder.py
+++ graph_builder.py
@@ -1,7 +1,12 @@
 import networkx as nx
+import ijson
+import gzip
 
+config.VERSION="20160502"
+config.PATH="/storage/datasets/"
 
 def build_graph():
+    parser = ijson.parse(bz2.open(config.PATH+'wikidata-'+config.VERSION+'-all.json.bz2', mode = 'rb'))
     G = nx.Graph()
     G.add_node(1)
     G.add_nodes_from([2,3])
